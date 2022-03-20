195513f15a350f1c3705a208151ab1cd90ae1c19
r78gmai <101959285+r78gmai@users.noreply.github.com>
2022-03-20 12:49:03 +0900

    Create programtest.cs
    programtest cs

---
+++ programtest.cs
@@ -0,0 +1,7 @@
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.Write("aa");
+Console.WriteLine("11111");
+
+Func<int, int> fnc = x => x * 2;
+Console.WriteLine( fnc(2) );
